96befbad8baac1cd3eee6d01d8a889e5e9e8be15
Manish Kumar <mk743953@gmail.com>
2026-08-07 14:31:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.6%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        
+        int prev = 1;
+        int curr = 2;
+
+        for(int i=2; i<n; i++){
+            int next = prev+curr;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+};
