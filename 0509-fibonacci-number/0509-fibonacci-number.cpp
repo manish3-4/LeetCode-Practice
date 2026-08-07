667078cd48c8aa5d3a655715e8d6ce126622cf55
Manish Kumar <mk743953@gmail.com>
2026-08-07 14:09:32 +0530

    Time: 3 ms (64.85%), Space: 7.8 MB (39.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,6 +4,14 @@ public:
         if(n==0) return 0;
         if(n==1) return 1;
 
-        return fib(n-2)+fib(n-1);
+        int dp[n+1];
+
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-2]+dp[i-1];
+        }
+         return dp[n];
     }
 };
